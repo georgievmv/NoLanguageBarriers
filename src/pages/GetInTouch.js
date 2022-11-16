c00f07d4dd39fc0bd91b04559513bd367bd9224b
georgievmv <96878439+georgievmv@users.noreply.github.com>
2022-11-16 18:41:28 +0200

    improve styles

--- src/pages/GetInTouch.js
+++ src/pages/GetInTouch.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useRef, useState } from "react";
 import styles from "./GetInTouch.module.css";
 import {
   phoneIcon,
@@ -12,6 +13,33 @@ import {
 } from "../assets/icons";
 import Contacts from "../components/Contacts";
 const GetInTouch = () => {
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const subjectRef = useRef();
+  const messageRef = useRef();
+  const [error, setError] = useState("");
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+    if (
+      nameRef.current.value &&
+      emailRef.current.value &&
+      subjectRef.current.value &&
+      messageRef.current.value
+    ) {
+      if (emailRef.current.value.includes("@")) {
+        console.log([
+          nameRef.current.value,
+          emailRef.current.value,
+          subjectRef.current.value,
+          messageRef.current.value,
+        ]);
+      } else {
+        setError("Email is not valid.");
+      }
+    } else setError("Please fill all fields.");
+  };
+
   return (
     <div id="getInTouch" className={styles.container}>
       <h1 className={styles.heading}>Get in touch!</h1>
@@ -50,10 +78,22 @@ const GetInTouch = () => {
         <div className={styles.separator}></div>
         <div className={styles.contactForm}>
           <p>Or fill in our contact form</p>
-          <form>
+          {error === "Email is not valid." ? (
+            <p className={styles.errorMessage}>{error}</p>
+          ) : (
+            ""
+          )}
+          {error === "Please fill all fields." ? (
+            <p className={styles.errorMessage}>{error}</p>
+          ) : (
+            ""
+          )}
+          <form onSubmit={formSubmitHandler}>
             <div className={styles.inputGroup}>
               <label htmlFor="name">Name:</label>
               <input
+                onChange={() => setError("")}
+                ref={nameRef}
                 placeholder="Enter your name here"
                 name="name"
                 type="text"
@@ -62,6 +102,8 @@ const GetInTouch = () => {
             <div className={styles.inputGroup}>
               <label htmlFor="email">Email:</label>
               <input
+                onChange={() => setError("")}
+                ref={emailRef}
                 placeholder="Enter your email adress here"
                 name="email"
                 type="text"
@@ -70,6 +112,8 @@ const GetInTouch = () => {
             <div className={styles.inputGroup}>
               <label htmlFor="subject">Subject:</label>
               <input
+                onChange={() => setError("")}
+                ref={subjectRef}
                 placeholder="Type your subject here"
                 name="subject"
                 type="text"
@@ -78,6 +122,8 @@ const GetInTouch = () => {
             <div className={styles.inputGroup}>
               <label htmlFor="message">Message:</label>
               <input
+                onChange={() => setError("")}
+                ref={messageRef}
                 placeholder="Type your message here"
                 name="message"
                 type="text"
